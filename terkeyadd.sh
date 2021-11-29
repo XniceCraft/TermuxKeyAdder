c51c3ac96206c46bde7c0e6758f843b1ae0601e3
XniceCraft <49149283+XniceCraft@users.noreply.github.com>
2021-11-30 06:44:42 +0700

    Add files via upload

--- terkeyadd.sh
+++ terkeyadd.sh
@@ -1,29 +1,6 @@
-#!/bin/sh
-#Dilarang Keras Untuk Recode!!
-#Don't Recode This Script!!
+#!/data/data/com.termux/files/usr/bin/bash
 
-printf "\033[1;33m[>] \033[1;32mLoading tools"; sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 1
-echo "\n\033[1;33m[>] \033[1;36mMembuat Setup"; sleep 1
-cd /data/data/com.termux/files/home
-mkdir .termux
-
-if [ -d "/data/data/com.termux/files/home/.termux" ]; then
-   echo "\033[1;32m[>] Sukses"; sleep 0.6
-   echo "\033[1;33m[>] \033[1;34mMembuat Config"; sleep 1
-   cd .termux
-   echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" > termux.properties
-   sleep 1
-   
-   if [ -f "/data/data/com.termux/files/home/.termux/termux.properties" ]; then
-      echo "\033[1;32m[>] Sukses"; sleep 0.5
-      echo "\033[1;33m[>] Harap Restart Termux \033[0m"
-  
-    else
-       echo "\033[1;31m[>] Gagal Membuat Config!, Harap Check Permission"
-       fi
-  
-  else
-       echo "\033[1;31m[>] Gagal Membuat Setup!, Harap Check Permission"
-       fi
-      
-
+echo -e "\e[1;33m[>] \e[1;36mCreating Folder"
+mkdir /data/data/com.termux/files/home/.termux 2>/dev/null && echo -e "\e[1;32m[=] Success" || echo -e "\e[1;31m[X] Failed to create .termux folder"
+echo -e "\e[1;33m[>] \e[1;34mCreating Config"
+echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" > /data/data/com.termux/files/home/.termux/termux.properties && echo -e "\e[1;32m[=] Success"; echo -e "\e[1;33m[>] Please restart your termux \e[0m" || echo -e "\e[1;31m[x] Failed to create the config!, please check folder permission"
